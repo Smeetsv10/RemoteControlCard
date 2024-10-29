730ec191b12b71d69a63d7ed9595a0b543793c24
Victor Smeets <victorsmeets.vs@gmail.com>
2024-10-29 22:32:00 +0100

    small change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"4cb4478016b5445b1d0bc8eafb4fbace128cd320","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"4cb4478016b5445b1d0bc8eafb4fbace128cd320","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3444537980"
+    serviceWorkerVersion: "1692935493"
   }
 });
