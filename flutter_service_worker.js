2bc28ba52f1ab5e1fbc7a11e8da9cacd78f44a48
Smeetsv10 <victorsmeets.vs@gmail.com>
2025-08-16 18:21:51 +0200

    updated version, isues with reloading

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cec559d8b74e1d9a2db6a53b7e11ff0f",
-"/": "cec559d8b74e1d9a2db6a53b7e11ff0f",
-"main.dart.js": "06df9e73bc305cefa2588c85b648bee5",
+"index.html": "6c77d3b9d39b5d43e698be836e4c2a91",
+"/": "6c77d3b9d39b5d43e698be836e4c2a91",
+"main.dart.js": "f719d774b85b6972652839e054e6787d",
 "manifest.json": "99f6b9f61fbaf3f68c51774661dd30c1",
 "version.json": "e2a49a0b1e0434995500fff1973b354a"};
 // The application shell files that are downloaded before a service worker can
