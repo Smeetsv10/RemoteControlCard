e46ab1be9cb120e2a2c2c78d3ae5b993dfe3b9b0
Victor Smeets <victorsmeets.vs@gmail.com>
2025-08-18 19:38:45 +0200

    updated issue scrollcontroller

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -30,3 +30,14 @@ eventListener = (message) => {
 addEventListener("message", eventListener);
 `],{type:"application/javascript"}))}return url},mainScriptUrlOrBlob:a})};var S=class{async loadEntrypoint(e){let{serviceWorker:r,...t}=e||{},n=new g,s=new h;s.setTrustedTypesPolicy(n.policy),await s.loadServiceWorker(r).catch(o=>{console.warn("Exception while loading service worker:",o)});let a=new v;return a.setTrustedTypesPolicy(n.policy),this.didCreateEngineInitializer=a.didCreateEngineInitializer.bind(a),a.loadEntrypoint(t)}async load({serviceWorkerSettings:e,onEntrypointLoaded:r,nonce:t,config:n}={}){n??={};let s=_flutter.buildConfig;if(!s)throw"FlutterLoader.load requires _flutter.buildConfig to be set";let a=n.wasmAllowList?.[p.browserEngine]??C[p.browserEngine],o=d=>{switch(d){case"skwasm":return p.supportsWasmGC&&a;default:return!0}},u=(d,b)=>d.renderer==b,w=d=>d.compileTarget==="dart2wasm"&&!p.supportsWasmGC||n.renderer&&!u(d,n.renderer)?!1:o(d.renderer),m=s.builds.find(w);if(!m)throw"FlutterLoader could not find a build compatible with configuration and environment.";let l={};l.flutterTT=new g,e&&(l.serviceWorkerLoader=new h,l.serviceWorkerLoader.setTrustedTypesPolicy(l.flutterTT.policy),await l.serviceWorkerLoader.loadServiceWorker(e).catch(d=>{console.warn("Exception while loading service worker:",d)}));let y=E(n,s);m.renderer==="canvaskit"?l.canvasKit=U(l,n,p,y):m.renderer==="skwasm"&&(l.skwasm=P(l,n,p,y));let f=new v;return f.setTrustedTypesPolicy(l.flutterTT.policy),this.didCreateEngineInitializer=f.didCreateEngineInitializer.bind(f),f.load(m,l,n,t,r)}};window._flutter||(window._flutter={});window._flutter.loader||(window._flutter.loader=new S);})();
 //# sourceMappingURL=flutter.js.map
+
+if (!window._flutter) {
+  window._flutter = {};
+}
+_flutter.buildConfig = {"engineRevision":"4ffa9852f9f07f0279fc3da0a1b7dfea184226fd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: "3899217227"
+  }
+});
