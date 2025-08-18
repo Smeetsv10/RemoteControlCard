e6b080325f9e42f48f65285aeff3f65bf92ac7e6
Victor Smeets <victorsmeets.vs@gmail.com>
2025-08-18 21:46:20 +0200

    loading screen app reset

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"4ffa9852f9f07f0279fc3da0a1b7dfea184226
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2799990516"
+    serviceWorkerVersion: "1461740384"
   }
 });
